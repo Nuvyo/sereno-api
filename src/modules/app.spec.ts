11506473e8c74da5717cb921348d04637f64ee5c
gsborgz <borgesgabrielsilva@gmail.com>
2025-11-12 19:27:45 -0300

    quebrando teste para testar ci

--- src/modules/app.spec.ts
+++ src/modules/app.spec.ts
@@ -38,8 +38,8 @@ describe('app', () => {
       assert.ok(body.dependencies);
       assert.ok(body.dependencies.database);
       assert.ok(body.dependencies.database.version);
-      assert.ok(typeof body.dependencies.database.max_connections === 'string');
-      assert.ok(typeof body.dependencies.database.opened_connections === 'number');
+      assert.equal(typeof body.dependencies.database.max_connections, 'string');
+      assert.equal(typeof body.dependencies.database.opened_connections, 'number');
       
       const updatedAt = new Date(body.updated_at);
   
